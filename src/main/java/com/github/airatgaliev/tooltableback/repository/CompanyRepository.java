2b4f5c2aa8bfc1bf03aa6c641919077897891c67
Airat Galiev <airat.galiev@gmail.com>
2020-06-07 19:48:13 +0300

    added repositories interfaces and application.properties

---
+++ src/main/java/com/github/airatgaliev/tooltableback/repository/CompanyRepository.java
@@ -0,0 +1,8 @@
+package com.github.airatgaliev.tooltableback.repository;
+
+import com.github.airatgaliev.tooltableback.entity.Company;
+import org.springframework.data.repository.CrudRepository;
+
+public interface CompanyRepository extends CrudRepository<Company, Long> {
+
+}
---
+++ src/main/java/com/github/airatgaliev/tooltableback/repository/StockRepository.java
@@ -0,0 +1,8 @@
+package com.github.airatgaliev.tooltableback.repository;
+
+import com.github.airatgaliev.tooltableback.entity.Stock;
+import org.springframework.data.repository.CrudRepository;
+
+public interface StockRepository extends CrudRepository<Stock, Long> {
+
+}
