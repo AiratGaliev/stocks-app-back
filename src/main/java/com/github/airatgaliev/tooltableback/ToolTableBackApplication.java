5232abc86f199af74b3c248b832eedf83f1685a4
Airat Galiev <airat.galiev@gmail.com>
2020-06-07 17:29:59 +0300

    start new project

---
+++ src/main/java/com/github/airatgaliev/tooltableback/ToolTableBackApplication.java
@@ -0,0 +1,13 @@
+package com.github.airatgaliev.tooltableback;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ToolTableBackApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(ToolTableBackApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/github/airatgaliev/tooltableback/ToolTableBackApplicationTests.java
@@ -0,0 +1,13 @@
+package com.github.airatgaliev.tooltableback;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ToolTableBackApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
